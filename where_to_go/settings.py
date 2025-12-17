051267cf8c040ae8c3dca5dcda9e05644d3211a2
Equ17y <antonsobol28@gmail.com>
2025-12-17 21:18:37 +0300

    add blank start page

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -54,10 +54,11 @@ ROOT_URLCONF = 'where_to_go.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
