dad2075ef4240b7bbc35d420614a41dfe89e5ce8
Equ17y <antonsobol28@gmail.com>
2025-12-27 19:47:19 +0300

    Inline images have been added to the admin panel for easy editing

--- places/admin.py
+++ places/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import Place, PlaceImage
 # Register your models here.
 
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+    extra = 1
+    ordering = ('position',)
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+    inlines = [PlaceImageInline]
     list_display = ('name', 'lat', 'lng')
     
 @admin.register(PlaceImage)
