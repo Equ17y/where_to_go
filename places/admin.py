e39adb3ede7652aa6ebd2943392cc51b7834b996
Equ17y <antonsobol28@gmail.com>
2025-12-29 23:51:03 +0300

    Added a table with images

--- places/admin.py
+++ places/admin.py
@@ -35,3 +35,24 @@ class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
         form = super().get_form(request, obj, **kwargs)
         form.base_fields["long_description"].widget = TinyMCE()
         return form
+    
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    list_display = ("image_preview", "place", "position")
+    list_filter = ("place",)
+    ordering = ("place", "position")
+    readonly_fields = ("image_preview",)
+    # 👇 КЛЮЧЕВОЕ: избегаем гигантского <select>
+    raw_id_fields = ("place",)  # или autocomplete_fields = ("place",)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-height: 100px; max-width: 150px; '
+                'width: auto; height: auto; border: 1px solid #ccc;"/>',
+                obj.image.url,
+            )
+        return "Нет изображения"
+
+    image_preview.short_description = "Превью"
+        
